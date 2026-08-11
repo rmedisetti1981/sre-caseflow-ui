4411049c82b5c15dc7c8cd0b193de1c085bee1fe
rmedisetti1981 <rmedisetti1981@gmail.com>
2026-08-12 03:32:51 +0530

    Added RTK Query to Redux store
    Created baseApi.ts : Instead of creating multiple independent RTK Query API slices, we'll use one base API and inject endpoints into it.

--- src/state/client/store.ts
+++ src/state/client/store.ts
@@ -1,11 +1,16 @@
 import { configureStore } from '@reduxjs/toolkit';
 
 import authReducer from './slices/authSlice';
+import { baseApi } from '../server/baseApi';
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
+    [baseApi.reducerPath]: baseApi.reducer,
   },
+
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(baseApi.middleware),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
---
+++ src/state/server/baseApi.ts
@@ -0,0 +1,20 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+export const baseApi = createApi({
+  reducerPath: 'api',
+
+  baseQuery: fetchBaseQuery({
+    baseUrl: '/api',
+  }),
+
+  tagTypes: [
+    'Claims',
+    'Claim',
+    'Users',
+    'Documents',
+    'Document',
+    'Operations',
+  ],
+
+  endpoints: () => ({}),
+});
